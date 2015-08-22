b8372300466c23bad8e6559afc433cf5c363b0ad
fantaandcrackers <arthur.shir@gmail.com>
2015-08-21 18:36:58 -0700

    Second COmmit

---
+++ HitupMe/CreateController.swift
@@ -0,0 +1,42 @@
+//
+//  CreateController.swift
+//  HitupMe
+//
+//  Created by Arthur Shir on 8/21/15.
+//  Copyright (c) 2015 HitupDev. All rights reserved.
+//
+
+import UIKit
+
+class CreateController: UIViewController {
+    
+    @IBAction func touchDone(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: {})
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HitupMe/HitupCell.swift
@@ -0,0 +1,24 @@
+//
+//  HitupCell.swift
+//  HitupMe
+//
+//  Created by Arthur Shir on 8/21/15.
+//  Copyright (c) 2015 HitupDev. All rights reserved.
+//
+
+import UIKit
+
+class HitupCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
