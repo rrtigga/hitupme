ea37b61f4b15752b14937300dea5a2b91aa84426
fantaandcrackers <arthur.shir@gmail.com>
2015-08-24 16:47:47 -0700

    Added CellTypes (Joined/Hosted/NotResponded)

--- HitupMe/HitupCell.swift
+++ HitupMe/HitupCell.swift
@@ -14,19 +14,42 @@ class HitupCell: UITableViewCell {
     @IBOutlet var profilePic: UIImageView!
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var HeaderLabel: UITextView!
+    @IBOutlet var typePic: UIImageView!
     // Logistical Labels
     @IBOutlet var locationLabel: UILabel!
     @IBOutlet var joinedLabel: UILabel!
     @IBOutlet var distanceLabel: UILabel!
     @IBOutlet var pastTimeLabel: UILabel!
     
+    enum cellType {
+        case Joined, Hosted, NotResponded
+    }
+    
+    var savedCellType = cellType.NotResponded
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        HeaderLabel.text = "ksdjfld"
-        // Initialization code
+        HeaderLabel.text = "Programming at Peet's Coffee, feel free to join"
+        locationLabel.text = "Peet's Coffee"
+        setCellType(savedCellType)
     }
+    
+    internal func setCellType(type: cellType) {
+        savedCellType = type
+        switch savedCellType {
+        case .Joined:
+            typePic.image = UIImage(named: "Cell_Joined")
+        case .Hosted:
+            typePic.image = UIImage(named: "Cell_Hosted")
+        case .NotResponded:
+            typePic.image = nil
+            
+        }
+        
+    }
+    
 
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
